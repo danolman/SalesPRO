fb57bfb894cbce65d340d9935f6a1d5e4422b85f
danolman <townsendaniel@gmail.com>
2014-10-20 19:10:39 -0300

    navegacion ionic

--- js/app/App.js
+++ js/app/App.js
@@ -1,6 +1,6 @@
 var App = angular.module("App", ["ionic"])
 
-App.controller("AppCtrl", function($scope, $ionicSideMenuDelegate, $ionicPopover) {
+App.controller("AppCtrl", function($scope, $ionicSideMenuDelegate, $ionicPopup, $ionicModal, $ionicNavBarDelegate) {
 	
 	$scope.todo = [
 		{"name": "Limpiar la cocina"},
@@ -19,15 +19,69 @@ App.controller("AppCtrl", function($scope, $ionicSideMenuDelegate, $ionicPopover
     	$ionicSideMenuDelegate.toggleRight();
  	};
 
- 	$ionicPopover.fromTemplateUrl('my-popover.html', {
-    scope: $scope,
-	  }).then(function(popover) {
-	    $scope.popover = popover;
+ 	 $scope.showAlert = function() {
+	   var alertPopup = $ionicPopup.alert({
+	     title: 'Don\'t eat that!',
+	     template: 'It might taste good'
+	   });
+	   alertPopup.then(function(res) {
+	     console.log('Thank you for not eating my delicious ice cream cone');
+	   });
+	 };
+
+	 //MODAL
+	 // Create and load the Modal
+	  $ionicModal.fromTemplateUrl('modal.html', function(modal) {
+	    $scope.taskModal = modal;
+	  }, {
+	    scope: $scope,
+	    animation: 'slide-in-up'
 	  });
- 	$scope.openPopover = function($event) {
- 		alert("holi");
-    	$scope.popover.show($event);
-  	};
+
+	  // Open our new task modal
+	  $scope.newTask = function() {
+	    $scope.taskModal.show();
+	  };
+
+	  // Close the new task modal
+	  $scope.closeNewTask = function() {
+	    $scope.taskModal.hide();
+	  };
+
+	   $scope.getPreviousTitle = function() {
+	    return $ionicNavBarDelegate.getPreviousTitle();
+	  };
+
+	  $scope.goBack = function() {
+	    $ionicNavBarDelegate.back();
+	  };
+
+});
+
+
+App.config( function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    	.state('login', 
+    	{
+    		url: '/',
+    		templateUrl: 'templates/login.html'
+    	})
+    	.state('home', 
+    	{
+    		url: '/home',
+    		templateUrl: 'templates/home.html'
+    	});
+/*$stateProvider
+	.state('home', {
+      url: "/home",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/home.html"
+        }
+      }
+    });*/
 });
 
 
